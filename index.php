4656a4d4e9a1a28e94467a7fd5b6255a8df22965
Gospodarstwo-ogrodnicze <65110123+Gospodarstwo-ogrodnicze@users.noreply.github.com>
2020-06-06 21:01:17 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gospodarstwo ogrodnicze</title>
+	<meta name="description" content="Gospodarstwo ogrodnicze Tomasz Dybalski" />
+	<meta name="keywords" content="kwiaty, ogród żarnów, żarnów, gmina żarnów, targ żarnów, " />
+    <link type="image/x-icon" rel="Shortcut icon" href="images/favicon.ico" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+    
+<body ondragstart="return false" ondrag="return false">
+    
+    <div class="header">	
+    <div class="logo">
+        <img src="images/ros.png" style="float: left;"/>
+        <span style="color: #00cc00">Godpodarstwo</span> ogrodnicze
+    <div style="clear:both;"></div>
+    </div>
+    </div>
+    
+<nav>
+    <ul class="menu">
+    <li><a href="./"><i class="fa fa-home"></i> Strona główna</a>
+    <li><a href="index.php?id=cennik"><i class="fa fa-tags"></i> Cennik</a></li>
+    <li><a href="index.php?id=aktualnosci"><i class="fa fa-bullhorn"></i> Aktualności</a>
+    <li><a href="index.php?id=kontakt"><i class="fa fa-envelope"></i> Kontakt</a></li>
+    <li><a href="index.php?id=onas"><i class="fa fa-user"></i> O nas</a>
+    </ul>
+</nav>
+    
+    <div class="content">
+    <?php if(!isset($_GET['id'])) $_GET['id']=null;
+    $open=$_GET['id'].'.php'; If(is_file($open) and $_GET['id']!= "index") include($open); else include('glowna.php');?>
+    </div>
+    
+    <div class="stopka">
+    <a style="font-size: 15px; font-weight: bold; text-align: center">Kontakt &nbsp|&nbsp<i class="icon-mail-alt"></i> 123@123.pl &nbsp|&nbsp Gospodarstwo ogrodnicze,Konecka 39 Żarnów 26-330</a>
+    </div>   
+    
+    <script>
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+    </script>
+    
+	<script src="skrypty/jquery.bpopup.min.js"></script>
+	<script>
+	$( document ).ready(function() {
+		$('#popup_this').bPopup();
+	});
+	</script>
+    
+</body>
+</html>
